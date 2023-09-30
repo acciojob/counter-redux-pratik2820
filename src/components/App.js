98af65ddb205cf86cba8520cb50603f9aee56baa
pratik2820 <pratikmorge28@gmail.com>
2023-09-30 17:13:33 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useDispatch,useSelector} from 'react-redux'
+import { increment,decrement } from "../redux/actions/counterActions";
 
 const App = () => {
+  let dispatch=useDispatch()
+  let x= useSelector((data)=>data)
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="1">
+      <h1 className="4">{x}</h1>
+      <button className="increment" onClick={()=>dispatch(increment())}>increment</button> 
+      <button className="decrement" onClick={()=>dispatch(decrement())}>decrement</button>
     </div>
   )
 }
